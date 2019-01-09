69c495395239697fa2dddfaf76a9ace918010004
Maks <mail@mymail.me>
2019-01-09 20:50:15 +0700

    Our first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let arr = ['one','two','three'];
+// // let answer = confirm('Are u here?')
+// let answer = prompt('Are u here?', 'Yes');
+// console.log(typeof(arr));
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
